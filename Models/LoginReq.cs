fb9169f5955263ec348e4fc1cf5eac71f91faaf9
christolx <81254243+christolx@users.noreply.github.com>
2025-03-08 12:50:38 +0700

    Added constraints to register & login model

--- Models/LoginReq.cs
+++ Models/LoginReq.cs
@@ -1,7 +1,11 @@
-﻿namespace ureeka_backend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ureeka_backend.Models;
 
 public class LoginReq
 {
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string Password { get; set; }
 }
--- Models/RegisterReq.cs
+++ Models/RegisterReq.cs
@@ -1,8 +1,13 @@
-﻿namespace ureeka_backend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ureeka_backend.Models;
 
 public class RegisterReq
 {
+    [Required]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string Username { get; set; } = string.Empty;
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
