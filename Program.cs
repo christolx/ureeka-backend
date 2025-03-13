2f25b0719b0628889953bd0e6663eb0d0251c575
christolx <81254243+christolx@users.noreply.github.com>
2025-03-14 00:31:41 +0700

    Configured CORS for Vercel Frontend

--- Program.cs
+++ Program.cs
@@ -24,7 +24,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
-        policy => policy.WithOrigins("http://localhost:5173")
+        policy => policy
+            .WithOrigins(
+                "http://localhost:5173", 
+                "https://foodfund-frontend.vercel.app"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
